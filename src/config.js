d314f5ff4daa481192de72398b997462b50266f3
dylanclarke890 <dylanclarke890@gmail.com>
2022-04-12 08:37:56 +0100

    added layer configs and debug logs

--- src/config.js
+++ src/config.js
@@ -1,11 +1,16 @@
-const { MODE } = require("./blendMode.js");
+"use strict";
+
+const path = require("path");
+const isLocal = typeof process.pkg === "undefined";
+const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
+const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
   "This is the description of your NFT project, remember to replace this";
-const baseUri = "ipfs://QmNfPMWLPTEbFpBtPFy4wkYEHRVWcz8dzjziTcPbebzF53";
+const baseUri = "ipfs://NewUriToReplace";
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 20,
+    growEditionSizeTo: 10,
     layersOrder: [
       { name: "top" },
       { name: "middle-one" },
@@ -14,6 +19,10 @@ const layerConfigurations = [
     ],
   },
 ];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
 
 const format = {
   width: 512,
@@ -25,16 +34,18 @@ const background = {
   brightness: "80%",
 };
 
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
 const preview = {
   thumbPerRow: 5,
   thumbWidth: 50,
   imageRatio: format.width / format.height,
   imageName: "preview.png",
 };
-
-const rarityDelimiter = "#";
-
-const uniqueDnaTorrance = 10000;
 
 module.exports = {
   format,
@@ -45,4 +56,7 @@ module.exports = {
   layerConfigurations,
   rarityDelimiter,
   preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
 };
